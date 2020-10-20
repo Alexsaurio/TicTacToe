8a2273d5f3a9e855ba7de154fd5cdecf45f64acb
Alejandro AR <alex-ar@outlook.com>
2020-10-19 21:18:36 -0500

    Updates

---
+++ precache-manifest.217cc4125940a150f77401c1ff862487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d8f532fa9b32ffa89f85b8237f1a8a",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "826fdd0a3af0b989b7bf",
+    "url": "/TicTacToe/static/css/main.12b1585e.chunk.css"
+  },
+  {
+    "revision": "a4270d2037db98e7794e",
+    "url": "/TicTacToe/static/js/2.a0320ff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToe/static/js/2.a0320ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826fdd0a3af0b989b7bf",
+    "url": "/TicTacToe/static/js/main.0bf6fccb.chunk.js"
+  },
+  {
+    "revision": "d60dcebf29b60cb7b919",
+    "url": "/TicTacToe/static/js/runtime-main.e35d1982.js"
+  }
+]);
